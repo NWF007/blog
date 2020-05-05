c7e44f62d2963152999e4bbc96409396f533f312
Nico <ncfortuin27@gmail.com>
2020-05-05 10:37:36 +0200

    First commit

---
+++ src/build/precache-manifest.5791bb81d55774a1c4b15d0f5d4a6a5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb9ac4c17191273d65d3f0a5bf44a07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d13c5a7d824e8c1de68e",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "a290bea3eb5fe5b74321",
+    "url": "/static/js/2.2b50056a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2b50056a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d13c5a7d824e8c1de68e",
+    "url": "/static/js/main.19087969.chunk.js"
+  },
+  {
+    "revision": "f54fef1e51797e0f30b5",
+    "url": "/static/js/runtime-main.4a75ee64.js"
+  }
+]);
